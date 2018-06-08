1bdcab0c2cf4ce63bd2afbdc1d88658ed0ca9d11
kgmyshin <kgmyshin82@gmail.com>
2018-06-08 17:39:16 +0900

    :sparkles: 各種dependencies追加
    issue #
    
    androidx系のlibraryを追加

--- app/src/main/java/com/kgmyshin/myapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/kgmyshin/myapplication/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.kgmyshin.myapplication.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.kgmyshin.myapplication.R
 
 class MainFragment : Fragment() {
@@ -24,7 +24,6 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
